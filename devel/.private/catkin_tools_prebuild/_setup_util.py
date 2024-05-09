d4600a3f43b1098fb771119d04d5ea0577d4bf7f
Wooepeke <Xavier.reparon@gmail.com>
2024-05-09 15:40:06 +0200

    Added slam toolbox and fixed some gazebo settings, also fixed the flag giving it 0 weight and changing the inertia of the mirte

--- devel/.private/catkin_tools_prebuild/_setup_util.py
+++ devel/.private/catkin_tools_prebuild/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/opt/ros/noetic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/xavier/Personal/ro47007_MDP/group7-epic-mirte-master-we-are-nr1/devel;/opt/ros/noetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
--- devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
+++ devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/xavier/Personal/ro47007_MDP/group7-epic-mirte-master-we-are-nr1/devel/.private/catkin_tools_prebuild/lib;/opt/ros/noetic/lib)
+    foreach(path /home/xavier/Personal/ro47007_MDP/group7-epic-mirte-master-we-are-nr1/devel/.private/catkin_tools_prebuild/lib;/home/xavier/Personal/ro47007_MDP/group7-epic-mirte-master-we-are-nr1/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
